775ae39a2b095b825d57681fa3b935f6f5046846
PaulinaLeszczelowska <73898654+PaulinaLeszczelowska@users.noreply.github.com>
2020-12-16 14:46:12 +0100

    Add files via upload

--- Code/Data_Acquisition_and_Understanding/emotions.py
+++ Code/Data_Acquisition_and_Understanding/emotions.py
@@ -1,8 +1,9 @@
 import csv
 import os
 import shutil
+import random
 
-target_dir = "Divided_Data"
+target_dir = "Divided_Data_random"
 source_dir = 'D:\AMIGOS'
 
 #create target directory
@@ -27,60 +28,78 @@ with open('D:\AMIGOS\emotions.csv', 'r',encoding='utf-8', newline='') as source_
         videoID = 'V_'+str(row[1])
         video2 = 'V'+str(row[1])
         print(userID," ",videoID)
-        #neutral
-        if row[2] == '1':
-            #copy files to target directory
-            shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[0]))
+        labels = list(map(int, row[2:]))
+        #choose random
+        if sum(labels)>1:
+            print(row)
+            indices = [i for i, x in enumerate(row[2:]) if x=='1']
+            indices = [x + 2 for x in indices]
+            chosen_emotion = random.choice(indices)-2
+            print(chosen_emotion+2,emotions_dirs[chosen_emotion])
+            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_EEG.csv"),
+                        os.path.join(source_dir, target_dir, emotions_dirs[chosen_emotion]))
             shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[0]))
+                        os.path.join(source_dir, target_dir, emotions_dirs[chosen_emotion]))
             shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[0]))
-        #disgust
-        if row[3] == '1':
-            shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[1]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[1]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[1]))
-        #happiness
-        if row[4] == '1':
-            shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[2]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[2]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[2]))
-        #surprise
-        if row[5] == '1':
-            shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[3]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[3]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[3]))
-        #anger
-        if row[6] == '1':
-            shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[4]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[4]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[4]))
-        #fear
-        if row[7] == '1':
-            shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[5]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[5]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[5]))
-        #sadness
-        if row[8] == '1':
-            shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[6]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[6]))
-            shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
-                        os.path.join(source_dir,target_dir,emotions_dirs[6]))
+                        os.path.join(source_dir, target_dir, emotions_dirs[chosen_emotion]))
+        #skip
+        # if sum(labels)>1:
+        #     continue
+        else:
+            #neutral
+            if row[2] == '1':
+                #copy files to target directory
+                shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[0]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[0]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[0]))
+            #disgust
+            if row[3] == '1':
+                shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[1]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[1]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[1]))
+            #happiness
+            if row[4] == '1':
+                shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[2]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[2]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[2]))
+            #surprise
+            if row[5] == '1':
+                shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[3]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[3]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[3]))
+            #anger
+            if row[6] == '1':
+                shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[4]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[4]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[4]))
+            #fear
+            if row[7] == '1':
+                shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[5]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[5]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[5]))
+            #sadness
+            if row[8] == '1':
+                shutil.copy(os.path.join(source_dir,'Processed_Data',userID,videoID,userID + "_" + video2 + "_EEG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[6]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_ECG.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[6]))
+                shutil.copy(os.path.join(source_dir, 'Processed_Data', userID, videoID, userID + "_" + video2 + "_GSR.csv"),
+                            os.path.join(source_dir,target_dir,emotions_dirs[6]))
